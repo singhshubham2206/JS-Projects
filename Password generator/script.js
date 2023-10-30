6bb8d24b553a2d4e1544b481141a4aa0c4d6714c
singhshubham2206 <118054694+singhshubham2206@users.noreply.github.com>
2023-10-30 23:23:46 +0530

    Add files via upload

---
+++ Password generator/script.js
@@ -0,0 +1,45 @@
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
+
+sliderValue.textContent = inputSlider.value ;
+inputSlider.addEventListener('input', ()=>{
+    sliderValue.textContent = inputSlider.value;
+})
+
+genBtn.addEventListener('click', ()=>{
+    passBox.value = generatePassword();
+})
+
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "!@#$%^&*";
+
+
+// FUNCTION TO GENERATE PASSWORD
+function generatePassword(){
+    let genPassword="";
+    let allChars ="";
+
+    allChars  += lowercase.checked ? lowerChars : "";
+    allChars  += uppercase.checked ? upperChars : "";
+    allChars  += numbers.checked ? allNumbers : "";
+    allChars  += symbols.checked ? allSymbols : "";
+
+    let i = 1;
+    while(i<=inputSlider.value){
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        i++;
+    }
+
+    return genPassword;
+
+
+}
