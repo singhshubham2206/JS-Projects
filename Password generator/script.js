f57840535c732d6b79b5e3c6724f2a822c687182
singhshubham2206 <118054694+singhshubham2206@users.noreply.github.com>
2023-11-03 11:24:02 +0530

    Add files via upload

--- Password generator/script.js
+++ Password generator/script.js
@@ -28,18 +28,26 @@ function generatePassword(){
     let genPassword="";
     let allChars ="";
 
-    allChars  += lowercase.checked ? lowerChars : "";
-    allChars  += uppercase.checked ? upperChars : "";
-    allChars  += numbers.checked ? allNumbers : "";
-    allChars  += symbols.checked ? allSymbols : "";
+    allChars  += lowercase.checked ? lowerChars : "";  // allchars will contains all the lowercase chars
+    allChars  += uppercase.checked ? upperChars : "";  // allchars will contains all the lowercase + uppercase chars
+    allChars  += numbers.checked ? allNumbers : "";    // allchars will contains all the lowercase + uppercase + numbers chars
+    allChars  += symbols.checked ? allSymbols : "";    // allchars will contains all the lowercase + uppercase + numbers + symbols chars
 
     let i = 1;
     while(i<=inputSlider.value){
-        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
+        genPassword += allChars.charAt(Math.floor(          // with ever value of i genpassword store a char in it that is at the random place in allchar 
+                        Math.random() * allChars.length));
         i++;
     }
 
     return genPassword;
-
-
 }
+
+// copy the password from the Bar 
+copyIcon.addEventListener('click', ()=>{
+    if(passBox.value != "" || passBox.value.lenth >=1){
+        navigator.clipboard.writeText(passBox.value);
+        copyIcon.title="Password Copied";
+        copyIcon.innerHTML="check";
+    }    
+})
